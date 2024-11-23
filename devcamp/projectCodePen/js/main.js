ad353c727900f3d6a3d328cab2e6b93b7a9341cd
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-11-23 21:04:30 +0100

    Update main.js
    Updated with function expressions and function declaration. Added the img transition logic.

--- devcamp/projectCodePen/js/main.js
+++ devcamp/projectCodePen/js/main.js
@@ -1,3 +1,18 @@
 function thisWorks() {
     alert('This action works!');
 }
+
+const image = document.querySelector('.exampleWithShadowBox');
+
+image.addEventListener('mouseover', () => {
+    image.src = '../img/example2.png';
+});
+
+image.addEventListener('mouseout', () => {
+    image.src = '../img/lmr7a8E.png';
+});
+
+image.addEventListener('click', () => {
+    alert('CodePen gives you a lightweight HTML-CSS-JS IDE!. Anyway, this a basic HTML/CSS/JS demo for a Bootcamp. Thanks.');
+    
+});
