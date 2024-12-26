c7fc8d7b1afbc2ac2db0c9cd1b7d918eec7f2155
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-12-26 03:15:36 +0100

    Create 01_03_DataTypes.java

---
+++ OpenBootcamp/BackEnd/JavaSE/01_03_DataTypes.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // 1.   Numbers
+
+        // 1.1  Integers
+        byte variableOne = 1;
+        short variableTwo = 100;
+        int variableThree = 100000;
+        long variableFour = 10000000000;
+        long phoneNumber = 666145474;
+
+        // 1.2  Decimals
+        float variableFive = 5.5f;
+        double variableSix = 10.5d;
+        variableSix = 20.5d;
+        double productPrice = 49.99d;
+
+        // 2.   Booleans
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        // 3.   Strings
+        char variableNine = 'a';
+        String variableTen = "Lorem ipsum dolor sit anet ...";
+        
+    }
+}
