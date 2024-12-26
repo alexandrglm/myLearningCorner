8580efffb126d208989d59814d512d6dc8637bcb
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-12-26 02:24:10 +0100

    Create 01_02_ByeWorld.java

---
+++ OpenBootcamp/BackEnd/JavaSE/01_02_ByeWorld.java
@@ -0,0 +1,27 @@
+package com.example;
+
+
+/**
+ * Test class to create a simply Java application
+ */
+public class ByeWorld {
+
+    /**
+     * This is a JAVADoc. Entrypoint to java application
+     * Lets the application xecution
+     * @param args - Entry arguments for the application
+     */
+    public static void main(String[] args) {
+        // This is a simple-line comment
+        System.out.println("ByeWorld.main");
+
+        /*
+        This is a multi-line comment, useful for planning step by step:
+        1- Step1. Create the function sum
+        2. Create number variable
+        3. Use sum function to get the sum of both numbers
+         */
+
+    }
+
+}
