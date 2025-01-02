96d9414bc82d67bdb033be2a0d86e00631c61c5e
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-01-02 09:22:33 +0100

    Update 01_03_DataTypes.java

--- OpenBootcamp/BackEnd/JavaSE/01_03_DataTypes.java
+++ OpenBootcamp/BackEnd/JavaSE/01_03_DataTypes.java
@@ -1,31 +1,41 @@
 package com.example;
 
-public class DataTypes {
+public class DataTypesExamples {
 
     public static void main(String[] args) {
 
-        // 1.   Numbers
+        /*
+         DATA TYPES USED
+
+         1      NUMBERS
+         1.1    Integers
+         1.2    Decimals
+
+         2      BOOLEANS
+
+         3      STRINGS
+         */
 
         // 1.1  Integers
-        byte variableOne = 1;
-        short variableTwo = 100;
-        int variableThree = 100000;
-        long variableFour = 10000000000;
-        long phoneNumber = 666145474;
+        byte    variableOne     = 100;          // Max 10² long
+        short   variableTwo     = 10000;        // Max 10³ long
+        int     variableThree   = 1000000000;   // Max. 10⁹ long
+        long    variableFour    = 1000000000;   // Max. 10⁹ long
 
         // 1.2  Decimals
-        float variableFive = 5.5f;
-        double variableSix = 10.5d;
-        variableSix = 20.5d;
-        double productPrice = 49.99d;
+        float   variableFive    = 5.5f;
+        double  variableSix     = 10.5d;
+        variableSix = 20.5d;    // As let as coding JS, but in Java: a variable has been redeclared like this
+        double  productPrice = 40.99d;  // An example
 
-        // 2.   Booleans
-        boolean variable7 = true;
-        boolean variable8 = false;
+        // 2    BOOLEANS
+        boolean variableSeven   = true;
+        boolean variableEight   = false;
 
-        // 3.   Strings
-        char variableNine = 'a';
-        String variableTen = "Lorem ipsum dolor sit anet ...";
+        // 3    STRING
+        char    variableNine    = 'a';
+        String  variableTen     = "Lorem ipsum dolor sit anet ...";
         
     }
+    
 }
