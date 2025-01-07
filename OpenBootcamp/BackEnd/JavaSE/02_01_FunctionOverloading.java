f9fb018fd808fb80cae3b9ae4245d53a8b0d5d91
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-01-07 20:06:36 +0100

    Create 02_01_FunctionOverloading.java

---
+++ OpenBootcamp/BackEnd/JavaSE/02_01_FunctionOverloading.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Function Overloading lets a method being duplicated as long as having different number/kind of params.
+ */
+public class FunctionOverloading {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int sum(int number1, int number2) {
+        return number1 + number2;
+
+    }
+
+    static int sum(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+
+    }
+
+}
