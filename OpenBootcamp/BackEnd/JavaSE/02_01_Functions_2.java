49803804a023bf3a94ff8f1a2ef24fba5da3c3b5
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-01-07 20:05:23 +0100

    Create 02_01_Functions_2.java

---
+++ OpenBootcamp/BackEnd/JavaSE/02_01_Functions_2.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Functions2 {
+
+    public static void main(String[] args) {
+
+        int results1 = sum(500, 20);
+        int results2 = sum(10, 60);
+        int results3 = sum(90, 30);
+        int results4 = sum(40, 50);
+    }
+
+    static int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+}
