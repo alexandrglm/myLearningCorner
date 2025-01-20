d356853933da303dbf9baecf71cd2f192b134d6f
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-01-20 14:48:48 +0100

    Update and rename A01_A02_practicing1.s to A01_A02_practicing2.s

--- z80asmmooc/contents/Course/MODULE_1:Sprite_in_machine_Code/A01_PRACTICE/A01_A02_practicing2.s
+++ z80asmmooc/contents/Course/MODULE_1:Sprite_in_machine_Code/A01_PRACTICE/A01_A02_practicing2.s
@@ -1,19 +1,21 @@
 ;; CPCtelera and this asm startup routine belongs to (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 
-; 20250120: This solid box is read with the String method, so it's simply shown glitched. But it's a good first approaching.
-; I have to learn about setting up the video memory for solid boxes.
-; Coordinates on screen, done.
-
+; 20250120_2: 
+; Now, the videoMode has been configured, but the square is still being called as a String method.
+; That can be proved by seeing results using mode 1 and above. ( | | | | | | | | ).
+; Surprisely, using mode 0 we can see a rectangle wit no | spaces | between each character
 
 .include "cpctelera.h.s"
+
 .area _DATA
 
-thecolour: .db #1
+colour: .db #3
 
 .area _CODE
 ;; THIS comes from a the main.s included in CPCtelera v.1.5, and will be used as the base
 
 .globl cpct_disableFirmware_asm
+.globl cpct_setVideoMode_asm
 .globl cpct_getScreenPtr_asm
 .globl cpct_drawSolidBox_asm
 
@@ -21,13 +23,15 @@ _main::
 
    	call cpct_disableFirmware_asm
 
-   	ld   de, #CPCT_VMEM_START_ASM
-   	ld    b, #24
-   	ld    c, #16
-   
+	ld	c, #0
+	call cpct_setVideoMode_asm
+
+   	ld   	de, #CPCT_VMEM_START_ASM
+   	ld    	b, #24
+   	ld    	c, #16
 	call cpct_getScreenPtr_asm
 
-   	ld   a, (thecolour)
+   	ld   a, (colour)
    	ld   c, #8
    	ld   b, #8
    	call cpct_drawSolidBox_asm
