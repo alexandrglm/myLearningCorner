3f6973a1fc51f522dd5f1c6b9e735077877c84fb
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-01-20 14:13:15 +0100

    Update A01_A02_practicing1.s

--- z80asmmooc/contents/Course/MODULE_1:Sprite_in_machine_Code/A01_PRACTICE/A01_A02_practicing1.s
+++ z80asmmooc/contents/Course/MODULE_1:Sprite_in_machine_Code/A01_PRACTICE/A01_A02_practicing1.s
@@ -1,30 +1,36 @@
 ;; CPCtelera and this asm startup routine belongs to (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 
+; 20250120: This solid box is read with the String method, so it's simply shown glitched. But it's a good first approaching.
+; I have to learn about setting up the video memory for solid boxes.
+; Coordinates on screen, done.
+
+
 .include "cpctelera.h.s"
 .area _DATA
 
-thecolour: .db #3
+thecolour: .db #1
 
 .area _CODE
 ;; THIS comes from a the main.s included in CPCtelera v.1.5, and will be used as the base
 
-.globl cpct_disableFirmware_asm   ;; mandatory
+.globl cpct_disableFirmware_asm
 .globl cpct_getScreenPtr_asm
 .globl cpct_drawSolidBox_asm
 
 _main::
 
-   call cpct_disableFirmware_asm
+   	call cpct_disableFirmware_asm
 
-   ld   d, #10
-   ld   e, #5
-   call cpct_getScreenPtr_asm
+   	ld   de, #CPCT_VMEM_START_ASM
+   	ld    b, #24
+   	ld    c, #16
+   
+	call cpct_getScreenPtr_asm
 
-   ld   a, (thecolour)
-   ld   c, #10
-   ld   b, #10
-   call cpct_drawSolidBox_asm
+   	ld   a, (thecolour)
+   	ld   c, #8
+   	ld   b, #8
+   	call cpct_drawSolidBox_asm
 
-;;   infinite loop to maintain in-screen what we coded.
 loop:
    jr    loop
