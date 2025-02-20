6219483113e4ae28ead022bfceff261035d32e26
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-20 19:39:25 +0100

    Testing new structures, new filetypes, and so on

--- inProgress/Basque-DevCamp/Full_Stack_2025/00-pandoc-Controler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find . -type f -name "*.markdown" | while read -r file; do
-
-    filename="${file%.md}"
-
-    pandoc "$file" -o "${filename}".pdf --from markdown --to pdf --pdf-engine=lualatex -V geometry:margin=1.5cm
-
-done
