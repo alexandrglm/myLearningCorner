89a557044be491f00300e6fb594f54813b43e4fc
Alexandr Gomez <alexnadrglm@users.noreply.github.com>
2025-06-11 23:21:19 +0200

    Updated UML Examples, 01 Structural -> Class Diagrams

---
+++ inProgress/Basque-DevCamp/Full_Stack_2025/MODULE_06/Module06-Part-3_UML-1/06_03_00_UML_Exercises_1/01_Class_Diagram.js
@@ -0,0 +1,72 @@
+// Class Diagram examples using JS
+
+/*
+
+CLASS DIAGRAM for class User
+
+- CLASS NAME:   Users
+- ATTRIBUTES:   username, password, Realname
+- METHODS:      userInfo(), sendLogin()
+
+So...
+
+Users
+--
++ username: String
+- password: String
++ realname: Fullname
+--
++ userInfo()
++ sendLogin()
+
+
+
+
+Fullname
+--
+- firstName:    String
+- surname:      String
++ username:     String
+--
++ showRecord(firstName, surname, username): String
+
+
+*/
+
+
+
+
+class Usuario {
+    constructor(username, password, nombreReal) {
+        this.username = username;
+        this.password = password;
+        this.nombreReal = nombreReal;
+    }
+
+    userInfo() {
+        return `USER -> ${this.username}\nNombre real: ${this.nombreReal.userRecord()}`;
+    }
+
+    sendLogin() {
+        return `Login to ${this.username} account ...`;
+    }
+}
+
+class NombreReal {
+    constructor(nombre, apellidos, username) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.username = username;
+    }
+
+    userRecord() {
+        console.debug('[DEBUG] La clase NombreReal ha sido usada');
+        return `${this.apellidos.toUpperCase()}, ${this.nombre}`;
+    }
+}
+
+let nombre = new NombreReal('Pepito', 'Perez Piscinas', 'pepito');
+let user = new Usuario('pepito', 'contraseñaDePepito1234', nombre);
+
+console.log(user.userInfo());
+console.log(user.sendLogin());
